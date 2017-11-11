ef680026665eea27ccab8bbd325d5a571a03fa66
unknown <martin.suffo@gmail.com>
2017-11-11 19:00:53 -0300

    a

---
+++ src/vista/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <link href="/static/css/loginregistro.css" rel="stylesheet" type="text/css" />
+  <title>Login</title>
+</head>
+
+<body>
+  <div>
+  <header>
+    <b>LOGIN</b>
+  </header>
+  <form id="formLogin" action="index.html" method="post">
+    Usuario <br>
+    <input type="text" name="txtusr" value="">
+    <br> <br>
+    Contraseña
+    <br>
+    <input type="password" name="" value="">
+    <br>
+    <br>
+    <button type="submit" name="button">
+        Ingresar
+      </button>
+    <br> <br>
+    <a href="registro.html">Registrate</a>
+  </form>
+</div>
+</body>
+
+</html>
